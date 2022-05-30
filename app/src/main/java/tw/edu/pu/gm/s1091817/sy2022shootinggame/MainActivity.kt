9fa421106130f9244064f0e3572ba387b53c8833
s1091817 <s1091817@pu.edu.tw>
2022-05-31 00:18:14 +0800

    v.3

--- app/src/main/java/tw/edu/pu/gm/s1091817/sy2022shootinggame/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1091817/sy2022shootinggame/MainActivity.kt
@@ -1,11 +1,14 @@
 package tw.edu.pu.gm.s1091817.sy2022shootinggame
 
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var game : Game
     var flag:Boolean = false
     lateinit var job : Job
+    lateinit var imgAuthor : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +25,24 @@ class MainActivity : AppCompatActivity() {
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
 
-        img.setOnClickListener({
-            if (flag){
+        imgAuthor = findViewById(R.id.imgAuthor)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.sy)
+            .circleCrop()
+            .override(800, 600)
+            .into(imgAuthor)
+
+        img.setOnClickListener {
+            if (flag) {
                 flag = false
                 img.setImageResource(R.drawable.start)
                 job.cancel()
-            }
-            else{
+            } else {
                 flag = true
                 img.setImageResource(R.drawable.stop)
                 job = GlobalScope.launch(Dispatchers.Main) {
-                    while(flag) {
+                    while (flag) {
                         delay(10)
                         var canvas: Canvas = game.surfaceHolder.lockCanvas()
                         game.drawSomething(canvas)
@@ -40,6 +51,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-        })
+        }
     }
 }
