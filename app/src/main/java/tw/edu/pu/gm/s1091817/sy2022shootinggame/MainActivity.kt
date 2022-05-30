cea5d1a3f3f00991c3f5564ac6f46fd261db22ba
s1091817 <s1091817@pu.edu.tw>
2022-05-30 22:54:36 +0800

    v.1

---
+++ app/src/main/java/tw/edu/pu/gm/s1091817/sy2022shootinggame/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.gm.s1091817.sy2022shootinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
